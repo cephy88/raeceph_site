1b2bcaa33fee52d6043b7396b153c532377022f3
unknown <you@example.com>
2020-10-10 22:38:43 +0800

    updated settings and req10

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='base'),
     path('resume/', views.resume, name='resume'),
-    path('base/', views.base, name='base'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-	return render(request, 'base/home.html')
 
 def resume(request):
 	return render(request, 'base/resume.html')
